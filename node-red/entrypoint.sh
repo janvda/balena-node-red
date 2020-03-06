cf641f13bd9c918f1303f7bdd729cd9fac58e593
janvda <jan@Jans-MBP.lan>
2020-03-06 18:07:02 +0100

    node-red folder copy pasted from balena-google-photos-kerstmis

---
+++ node-red/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#echo "Allowing node-red to write to folder /data ..."
+#sudo chown node-red:node-red /data
+
+echo "Allowing node-red to write to folder /data ..."
+sudo chown node-red:node-red /data
+
+echo "Starting node-red ..."
+npm start -- --userDir /data
